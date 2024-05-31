e3ea86b5895b891030cb805f4b2652afbbd26b50
angelo <uns.248@gmail.com>
2024-06-01 02:53:00 +0800

    refactored

--- go.mod
+++ go.mod
@@ -1,8 +1,14 @@
 module github.com/angelo1121/downloader
 
+go 1.22.3
+
 require (
 	github.com/dustin/go-humanize v1.0.0
-	github.com/gosuri/uilive v0.0.0-20170323041506-ac356e6e42cd // indirect
 	github.com/gosuri/uiprogress v0.0.0-20170224063937-d0567a9d84a1
+)
+
+require (
+	github.com/gosuri/uilive v0.0.0-20170323041506-ac356e6e42cd // indirect
 	github.com/mattn/go-isatty v0.0.6 // indirect
+	golang.org/x/sys v0.0.0-20190222072716-a9d3bda3a223 // indirect
 )
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strconv"
 	"sync"
 	"time"
 
@@ -22,46 +23,35 @@ const (
 	statusDone        downloadStatus = "done"
 )
 
-const refreshRate = time.Millisecond * 1000
-const barLength = 100
+const (
+	refreshRate = time.Second
+	barLength   = 100
+)
 
 type passThru struct {
-	r io.Reader
-
-	barTotal uint8
-
+	r           io.Reader
 	total       uint64
 	denominator uint64
-
-	mux *sync.RWMutex
 }
 
 type downloader struct {
-	pt *passThru
-
-	bar *uiprogress.Bar
-
-	url      string
-	filename string
-	status   downloadStatus
-
+	pt            *passThru
+	bar           *uiprogress.Bar
+	url           string
+	filename      string
 	contentLength uint64
-
-	done chan bool
-
-	timeStarted time.Time
-	timeEnded   time.Time
+	done          chan bool
+	timeStarted   time.Time
+	timeEnded     time.Time
+	status        downloadStatus
 }
 
-func newDownloader(url string, filename string, p *uiprogress.Progress) *downloader {
+func newDownloader(url, filename string, p *uiprogress.Progress) *downloader {
 	done := make(chan bool)
-
 	bar := p.AddBar(barLength).AppendCompleted()
 	bar.Empty = '_'
 
-	var mux sync.RWMutex
-	pt := &passThru{mux: &mux}
-
+	pt := &passThru{}
 	d := &downloader{
 		pt:       pt,
 		done:     done,
@@ -72,23 +62,12 @@ func newDownloader(url string, filename string, p *uiprogress.Progress) *downloa
 	}
 
 	bar.PrependFunc(func(b *uiprogress.Bar) string {
-		mux.Lock()
-		defer mux.Unlock()
-
-		out := fmt.Sprintf("%s/%s", humanize.Bytes(pt.total), humanize.Bytes(d.contentLength))
-
-		return strutil.Resize(out, 15)
+		return strutil.Resize(fmt.Sprintf("%s/%s", humanize.Bytes(pt.total), humanize.Bytes(d.contentLength)), 15)
 	})
 	bar.PrependFunc(func(b *uiprogress.Bar) string {
-		mux.Lock()
-		defer mux.Unlock()
-
 		return strutil.Resize(string(d.status), 12)
 	})
 	bar.AppendFunc(func(b *uiprogress.Bar) string {
-		mux.Lock()
-		defer mux.Unlock()
-
 		switch d.status {
 		case statusDownloading:
 			return strutil.Resize(strutil.PrettyTime(time.Since(d.timeStarted)), 5)
@@ -99,85 +78,73 @@ func newDownloader(url string, filename string, p *uiprogress.Progress) *downloa
 		}
 	})
 
-	fmt.Print()
-	bar.Set(0)
-
 	return d
 }
 
 func (d *downloader) start() {
-	resp, err := http.Get(d.url)
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", d.url, nil)
 	if err != nil {
-		panic(err)
+		fmt.Println("Request Error:", err)
+		return
+	}
+
+	req.Header.Set("User-Agent", "Mozilla/5.0")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Get Error:", err)
+		return
 	}
 	defer resp.Body.Close()
 
-	pt := d.pt
+	if resp.StatusCode != http.StatusOK {
+		fmt.Println("HTTP Error:", resp.Status)
+		return
+	}
 
-	pt.mux.Lock()
-	d.contentLength = uint64(resp.ContentLength)
-	pt.mux.Unlock()
+	contentLength := resp.Header.Get("Content-Length")
+	if contentLength != "" {
+		size, _ := strconv.ParseInt(contentLength, 10, 64)
+		d.contentLength = uint64(size)
+		d.pt.denominator = d.contentLength / barLength
+	} else {
+		fmt.Println("Content-Length header is not set. Unable to determine content size before reading.")
+	}
 
-	pt.r = resp.Body
-
-	pt.denominator = d.contentLength / barLength
+	d.pt.r = resp.Body
+	d.timeStarted = time.Now()
+	d.status = statusDownloading
 
 	go func() {
-		pt.mux.Lock()
-		d.timeStarted = time.Now()
-		d.status = statusDownloading
-		pt.mux.Unlock()
-
-		if err := DownloadFile(pt, d.filename, d.done); err != nil {
-			panic(err)
+		if err := downloadFile(d.pt, d.filename, d.done); err != nil {
+			fmt.Println("Download Error:", err)
 		}
 	}()
 
 	for {
 		select {
 		case <-time.After(refreshRate):
-			pt.mux.Lock()
-			d.bar.Set(int(pt.barTotal))
-			pt.mux.Unlock()
-
+			d.bar.Set(int(d.pt.total / d.pt.denominator))
 		case <-d.done:
 			d.bar.Set(barLength)
-
-			pt.mux.Lock()
-			pt.total = d.contentLength
+			d.pt.total = d.contentLength
 			d.status = statusDone
 			d.timeEnded = time.Now()
-			pt.mux.Unlock()
-
 			return
 		}
 	}
 }
 
-// Read implements io.Reader
 func (pt *passThru) Read(p []byte) (int, error) {
 	n, err := pt.r.Read(p)
-
 	if err == nil {
-		pt.mux.Lock()
-		defer pt.mux.Unlock()
-
 		pt.total += uint64(n)
-
-		barTotal := pt.total / pt.denominator
-
-		if barTotal <= 1 {
-			pt.barTotal = 1
-		} else {
-			pt.barTotal = uint8(barTotal)
-		}
 	}
-
 	return n, err
 }
 
-// DownloadFile downloads a file.
-func DownloadFile(pt io.Reader, filepath string, done chan bool) error {
+func downloadFile(pt io.Reader, filepath string, done chan bool) error {
 	out, err := os.Create(filepath)
 	if err != nil {
 		return err
@@ -199,39 +166,25 @@ func main() {
 
 	var wg sync.WaitGroup
 
-	d1 := newDownloader("http://ipv4.download.thinkbroadband.com/10MB.zip", "test.zip", p)
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		d1.start()
-	}()
+	downloaders := []struct {
+		url      string
+		filename string
+	}{
+		{"http://ipv4.download.thinkbroadband.com/10MB.zip", "test1.zip"},
+		{"http://ipv4.download.thinkbroadband.com/5MB.zip", "test2.zip"},
+		{"http://ipv4.download.thinkbroadband.com/5MB.zip", "test3.zip"},
+		{"http://ipv4.download.thinkbroadband.com/5MB.zip", "test4.zip"},
+	}
 
-	d2 := newDownloader("http://ipv4.download.thinkbroadband.com/5MB.zip", "test2.zip", p)
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		d2.start()
-	}()
-
-	d3 := newDownloader("http://ipv4.download.thinkbroadband.com/5MB.zip", "test2.zip", p)
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		time.Sleep(time.Second * 5)
-		d3.start()
-	}()
-
-	d4 := newDownloader("http://ipv4.download.thinkbroadband.com/5MB.zip", "test2.zip", p)
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		time.Sleep(time.Second * 5)
-		d4.start()
-	}()
+	for _, d := range downloaders {
+		wg.Add(1)
+		go func(url, filename string) {
+			defer wg.Done()
+			newDownloader(url, filename, p).start()
+		}(d.url, d.filename)
+	}
 
 	p.SetRefreshInterval(refreshRate)
-
 	wg.Wait()
-
 	p.Stop()
 }
